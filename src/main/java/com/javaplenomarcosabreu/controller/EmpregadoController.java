ee37f4f6523c1610fbcad155531443ca079728d2
marcos <marcos@marcos-VPCCA15FB>
2017-07-20 23:19:30 -0300

    Iniciando a etapa de atualização de dados

--- src/main/java/com/javaplenomarcosabreu/controller/EmpregadoController.java
+++ src/main/java/com/javaplenomarcosabreu/controller/EmpregadoController.java
@@ -1,16 +1,25 @@
 package com.javaplenomarcosabreu.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.javaplenomarcosabreu.auxiliar.Auxiliar;
 import com.javaplenomarcosabreu.dao.DepartamentoDao;
+import com.javaplenomarcosabreu.dao.EmpregadoDao;
+import com.javaplenomarcosabreu.model.Departamento;
 import com.javaplenomarcosabreu.model.Empregado;
 
 @Controller
@@ -18,20 +27,22 @@ public class EmpregadoController {
 
 	@Autowired
 	Auxiliar auxiliar;
+	
+	@Autowired
+	Departamento departamento;	
 
 	DepartamentoDao depDao = new DepartamentoDao();
+	
+	EmpregadoDao empregadoDao;
 
 	@RequestMapping(value = "/cadastro", method = RequestMethod.GET)
-	public ModelAndView cadastro(Empregado empregado) {
+	public ModelAndView cadastro(Empregado empregado, ModelMap modelMap) {
 
 		ModelAndView pagina = new ModelAndView("cadastro");
 
-		try {
+		try {			 			
+			
 			pagina.addObject("mensagem", "Página de Cadastro");
-
-			empregado.setDepartamentos(depDao.obterDepartamentos());
-
-			/* pagina.addObject("empregado", empregado); */
 
 			pagina.addObject("pagina", "cadastro");
 
@@ -42,10 +53,11 @@ public class EmpregadoController {
 		return pagina;
 
 	}
-
+		
 	@RequestMapping(value = "/cadastro", method = RequestMethod.POST)
-	public ModelAndView cadastrar(@Valid Empregado empregado, BindingResult result) {
-
+	public ModelAndView cadastrar(@Valid @ModelAttribute Empregado empregado, @Valid Departamento departamento,			
+			BindingResult result, Model model, RedirectAttributes redirectAttributes) {
+				
 		ModelAndView pagina = null;
 
 		try {
@@ -57,30 +69,19 @@ public class EmpregadoController {
 				pagina.addObject("pagina", "cadastro");
 
 				pagina.addObject("mensagem", "Erro ao se tentar cadastrar o empregado");
-
-				empregado.setDepartamentos(depDao.obterDepartamentos());
-
-			} else if (empregado.getCodDepartamento() == null || empregado.getCodDepartamento().equals("")) {
-
-				result.rejectValue("codDepartamento", "error.codDepartamento", "O departamento do empregado deve ser selecionado");
+				 
+			} else {				
 				
-				empregado.setDepartamentos(depDao.obterDepartamentos());
+				empregadoDao = new EmpregadoDao();
 				
-				pagina = new ModelAndView("cadastro");
-
-				pagina.addObject("pagina", "cadastro");
+				/*empregadoDao.persist(empregado);*/
 				
-				pagina.addObject("mensagem", "Erro ao se tentar cadastrar o empregado");
+				redirectAttributes.addFlashAttribute("empregado", empregado); 
 				
-			} else {
-
-				empregado.setDataCadastro(auxiliar.gerarDataEhoraAtual());
-
-				pagina = new ModelAndView("sucesso");
-
-				pagina.addObject("empregado", empregado);
-
-				pagina.addObject("mensagem", "Cadastro realizado com sucesso");
+				redirectAttributes.addFlashAttribute("departamento", departamento);
+				
+				pagina = new ModelAndView("forward:/cadastro/selecionar-depto");
+	
 			}
 
 		} catch (Exception e) {
@@ -89,4 +90,28 @@ public class EmpregadoController {
 
 		return pagina;
 	}
+	
+	@RequestMapping(value = "/atualizar-dados", method=RequestMethod.GET)
+	public ModelAndView atualizarDados(@ModelAttribute Departamento departamento){
+			
+		ModelAndView pagina = null;
+		
+		try {
+			
+			List<Departamento>departamentos = new ArrayList<>(depDao.obterDepartamentos());
+			
+			pagina = new ModelAndView("atualizar-dados");
+			
+			pagina.addObject("departamento", departamentos);
+			
+			pagina.addObject("mensagem", "Página de atualização de dados cadastrais");
+									
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return pagina;
+		
+	}
+	
 }
